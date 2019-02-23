dc62dbe64fe3af212258ebe4ac595535e90f4d5b
afayou <1147753060@qq.com>
2019-02-23 18:08:22 +0800

    Initial commit

---
+++ model.py
@@ -0,0 +1,395 @@
+import tensorflow as tf
+import tensorflow.contrib.layers as layers
+import collections
+slim = tf.contrib.slim
+
+# 定义Block
+class Block(collections.namedtuple('Block', ['scope', 'unit_fn', 'args'])):
+    'A named tuple describing a ResNet block'
+
+
+# 定义降采样subsample方法
+def subsample(inputs, factor, scope=None):
+    if factor == 1:
+        return inputs
+    else:
+        return slim.max_pool2d(inputs, [1, 1], stride=factor, scope=scope)
+
+
+# 定义conv2d_same函数创建卷积层
+def conv2d_same(inputs, num_outputs, kernel_size, stride, scope=None):
+    if stride == 1:
+        return slim.conv2d(inputs, num_outputs, kernel_size, stride=1,
+                           padding='SAME', scope=scope)
+    else:
+        # kernel_size_effective = kernel_size + (kernel_size - 1) * (rate - 1)
+        pad_total = kernel_size - 1
+        pad_beg = pad_total // 2
+        pad_end = pad_total - pad_beg
+        inputs = tf.pad(inputs,
+                        [[0, 0], [pad_beg, pad_end], [pad_beg, pad_end], [0, 0]])
+        return slim.conv2d(inputs, num_outputs, kernel_size, stride=stride,
+                           padding='VALID', scope=scope)
+
+
+@slim.add_arg_scope
+# 定义堆叠Blocks函数，两层循环
+def stack_blocks_dense(net, blocks,
+                       outputs_collections=None):
+    for block in blocks:
+        with tf.variable_scope(block.scope, 'block', [net]) as sc:
+            for i, unit in enumerate(block.args):
+                with tf.variable_scope('unit_%d' % (i + 1), values=[net]):
+                    unit_depth, unit_depth_bottleneck, unit_stride = unit
+                    net = block.unit_fn(net,
+                                        depth=unit_depth,
+                                        depth_bottleneck=unit_depth_bottleneck,
+                                        stride=unit_stride)
+            net = slim.utils.collect_named_outputs(outputs_collections, sc.name, net)
+
+    return net
+
+
+# 创建ResNet通用arg_scope，定义函数默认参数值
+def resnet_arg_scope(is_training=True,
+                     weight_decay=0.0001,
+                     batch_norm_decay=0.997,
+                     batch_norm_epsilon=1e-5,
+                     batch_norm_scale=True):
+    batch_norm_params = {
+        'is_training': is_training,
+        'decay': batch_norm_decay,
+        'epsilon': batch_norm_epsilon,
+        'scale': batch_norm_scale,
+        'updates_collections': tf.GraphKeys.UPDATE_OPS,
+    }
+
+    with slim.arg_scope(
+            [slim.conv2d],
+            weights_regularizer=slim.l2_regularizer(weight_decay),
+            weights_initializer=slim.variance_scaling_initializer(),
+            activation_fn=tf.nn.relu,
+            normalizer_fn=slim.batch_norm,
+            normalizer_params=batch_norm_params):
+        with slim.arg_scope([slim.batch_norm], **batch_norm_params):
+            with slim.arg_scope([slim.max_pool2d], padding='SAME') as arg_sc:
+                return arg_sc
+
+
+@slim.add_arg_scope
+# 定义核心bottleneck残差学习单元
+def bottleneck(inputs, depth, depth_bottleneck, stride,
+               outputs_collections=None, scope=None):
+    with tf.variable_scope(scope, 'bottleneck_v2', [inputs]) as sc:
+        depth_in = slim.utils.last_dimension(inputs.get_shape(), min_rank=4)
+        preact = slim.batch_norm(inputs, activation_fn=tf.nn.relu, scope='preact')
+        if depth == depth_in:
+            shortcut = subsample(inputs, stride, 'shortcut')
+        else:
+            shortcut = slim.conv2d(preact, depth, [1, 1], stride=stride,
+                                   normalizer_fn=None, activation_fn=None,
+                                   scope='shortcut')
+
+        residual = slim.conv2d(preact, depth_bottleneck, [1, 1], stride=1,
+                               scope='conv1')
+        residual = conv2d_same(residual, depth_bottleneck, 3, stride,
+                               scope='conv2')
+        residual = slim.conv2d(residual, depth, [1, 1], stride=1,
+                               normalizer_fn=None, activation_fn=None,
+                               scope='conv3')
+
+        output = shortcut + residual
+
+        return slim.utils.collect_named_outputs(outputs_collections,
+                                                sc.name,
+                                                output)
+
+
+# 定义生成ResNet V2的主函数
+def resnet_v2(inputs,
+              blocks,
+              num_classes=None,
+              global_pool=True,
+              include_root_block=True,
+              reuse=None,
+              scope=None):
+    with tf.variable_scope(scope, 'resnet_v2', [inputs], reuse=reuse) as sc:
+        end_points_collection = sc.original_name_scope + '_end_points'
+        with slim.arg_scope([slim.conv2d, bottleneck,
+                             stack_blocks_dense],
+                            outputs_collections=end_points_collection):
+            net = inputs
+            if include_root_block:
+                with slim.arg_scope([slim.conv2d],
+                                    activation_fn=None, normalizer_fn=None):
+                    net = conv2d_same(net, 64, 7, stride=2, scope='conv1')
+                net = slim.max_pool2d(net, [3, 3], stride=2, scope='pool1')
+            net = stack_blocks_dense(net, blocks)
+
+            net = slim.batch_norm(net, activation_fn=tf.nn.relu, scope='postnorm')
+            if global_pool:
+                # Global average pooling.
+                net = tf.reduce_mean(net, [1, 2], name='pool5', keepdims=True)
+            if num_classes is not None:
+                net = slim.conv2d(net, num_classes, [1, 1], activation_fn=None,
+                                  normalizer_fn=None, scope='logits')
+            # Convert end_points_collection into a dictionary of end_points.
+            end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+            if num_classes is not None:
+                end_points['predictions'] = slim.softmax(net, scope='predictions')
+
+            return net, end_points
+
+
+# 设计层数为50的ResNet V2
+def resnet_v2_50(inputs,
+                 num_classes=None,
+                 global_pool=True,
+                 reuse=None,
+                 scope='resnet_v2_50'):
+    blocks = [
+        Block('block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]),
+        Block(
+            'block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)]),
+        Block(
+            'block3', bottleneck, [(1024, 256, 1)] * 5 + [(1024, 256, 2)]),
+        Block(
+            'block4', bottleneck, [(2048, 512, 1)] * 3)]
+    return resnet_v2(inputs, blocks, num_classes, global_pool,
+                     include_root_block=True, reuse=reuse, scope=scope)
+
+
+# 设计101层的ResNet V2
+def resnet_v2_101(inputs,
+                  num_classes=None,
+                  global_pool=True,
+                  reuse=None,
+                  scope='resnet_v2_101'):
+    blocks = [
+        Block(
+            'block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]),
+        Block(
+            'block2', bottleneck, [(512, 128, 1)] * 3 + [(512, 128, 2)]),
+        Block(
+            'block3', bottleneck, [(1024, 256, 1)] * 22 + [(1024, 256, 2)]),
+        Block(
+            'block4', bottleneck, [(2048, 512, 1)] * 3)]
+    return resnet_v2(inputs, blocks, num_classes, global_pool,
+                     include_root_block=True, reuse=reuse, scope=scope)
+
+
+# 设计152层的ResNet V2
+def resnet_v2_152(inputs,
+                  num_classes=None,
+                  global_pool=True,
+                  reuse=None,
+                  scope='resnet_v2_152'):
+    blocks = [
+        Block(
+            'block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]),
+        Block(
+            'block2', bottleneck, [(512, 128, 1)] * 7 + [(512, 128, 2)]),
+        Block(
+            'block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)]),
+        Block(
+            'block4', bottleneck, [(2048, 512, 1)] * 3)]
+    return resnet_v2(inputs, blocks, num_classes, global_pool,
+                     include_root_block=True, reuse=reuse, scope=scope)
+
+
+# 设计200层的ResNet V2
+def resnet_v2_200(inputs,
+                  num_classes=None,
+                  global_pool=True,
+                  reuse=None,
+                  scope='resnet_v2_200'):
+    blocks = [
+        Block(
+            'block1', bottleneck, [(256, 64, 1)] * 2 + [(256, 64, 2)]),
+        Block(
+            'block2', bottleneck, [(512, 128, 1)] * 23 + [(512, 128, 2)]),
+        Block(
+            'block3', bottleneck, [(1024, 256, 1)] * 35 + [(1024, 256, 2)]),
+        Block(
+            'block4', bottleneck, [(2048, 512, 1)] * 3)]
+    return resnet_v2(inputs, blocks, num_classes, global_pool,
+                     include_root_block=True, reuse=reuse, scope=scope)
+
+# 定义函数创建卷积层的函数，tensor，名称，卷积核的高宽，扫描步长的高宽，p为参数列表
+def conv_op(input_op, name, kh, kw, n_out, dh, dw, p):
+    n_in = input_op.get_shape()[-1].value
+
+    with tf.name_scope(name) as scope:
+        kernel = tf.get_variable(scope + "w",
+                                 shape=[kh, kw, n_in, n_out], dtype=tf.float32,
+                                 initializer=tf.contrib.layers.xavier_initializer_conv2d())
+        conv = tf.nn.conv2d(input_op, kernel, (1, dh, dw, 1), padding='SAME')
+        bias_init_val = tf.constant(0.0, shape=[n_out], dtype=tf.float32)
+        biases = tf.Variable(bias_init_val ,trainable=True, name='b')
+        z = tf.nn.bias_add(conv, biases)
+        activation = tf.nn.relu(z, name=scope)
+        p += [kernel, biases]
+        return activation
+
+# 定义创建全连接层的函数
+def fc_op(input_op, name, n_out, p):
+    n_in = input_op.get_shape()[-1].value
+
+    with tf.name_scope(name) as scope:
+        kernel = tf.get_variable(scope + "w", shape=[n_in, n_out], dtype=tf.float32,
+                                 initializer=tf.contrib.layers.xavier_initializer_conv2d())
+        biases = tf.Variable(tf.constant(0.1, shape=[n_out], dtype=tf.float32), name='b')
+        activation = tf.nn.relu_layer(input_op, kernel, biases, name=scope)
+        p +=[kernel, biases]
+        return activation
+
+# 定义最大池化层创建函数
+def mpool_op(input_op, name, kh, kw, dh, dw):
+    return tf.nn.max_pool(input_op, ksize=[1, kh, kw, 1],
+                          strides=[1, dh, dw, 1],
+                          padding='SAME',
+                          name=name)
+
+# 定义创建VGG-16网络结构的函数
+def inference_op(input_op, keep_prob):
+    p = []
+
+    conv1_1 = conv_op(input_op, name="conv1_1", kh=3, kw=3, n_out=64, dh=1, dw=1, p=p)
+    conv1_2 = conv_op(conv1_1, name="conv1_2", kh=3, kw=3, n_out=64, dh=1, dw=1, p=p)
+    pool1 = mpool_op(conv1_2, name="pool1", kh=2, kw=2, dw=2, dh=2)
+
+    conv2_1 = conv_op(pool1, name="conv2_1", kh=3, kw=3, n_out=128, dh=1, dw=1, p=p)
+    conv2_2 = conv_op(conv2_1, name="conv2_2", kh=3, kw=3, n_out=128, dh=1, dw=1, p=p)
+    pool2 = mpool_op(conv2_2, name="pool2", kh=2, kw=2, dw=2, dh=2)
+
+    conv3_1 = conv_op(pool2, name="conv3_1", kh=3, kw=3, n_out=256, dh=1, dw=1, p=p)
+    conv3_2 = conv_op(conv3_1, name="conv3_2", kh=3, kw=3, n_out=256, dh=1, dw=1, p=p)
+    conv3_3 = conv_op(conv3_2, name="conv3_3", kh=3, kw=3, n_out=256, dh=1, dw=1, p=p)
+    pool3 = mpool_op(conv3_3, name="pool3", kh=2, kw=2, dw=2, dh=2)
+
+    conv4_1 = conv_op(pool3, name="conv4_1", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    conv4_2 = conv_op(conv4_1, name="conv4_2", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    conv4_3 = conv_op(conv4_2, name="conv4_3", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    pool4 = mpool_op(conv4_3, name="pool4", kh=2, kw=2, dw=2, dh=2)
+
+    conv5_1 = conv_op(pool4, name="conv5_1", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    conv5_2 = conv_op(conv5_1, name="conv5_2", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    conv5_3 = conv_op(conv5_2, name="conv5_3", kh=3, kw=3, n_out=512, dh=1, dw=1, p=p)
+    pool5 = mpool_op(conv5_3, name="pool5", kh=2, kw=2, dw=2, dh=2)
+    # 将五段卷积网络的输出结果扁平化，变成一维向量
+    shp = pool5.get_shape()
+    flattened_shape = shp[1].value * shp[2].value * shp[3].value
+    resh1 = tf.reshape(pool5, [-1, flattened_shape], name="resh1")
+    #　创建一个隐含节点为4096的全连接层
+    fc6 = fc_op(resh1, name="fc6", n_out=4096, p=p)
+    fc6_drop = tf.nn.dropout(fc6, keep_prob, name="fc6_drop")
+
+    fc7 = fc_op(fc6_drop, name="fc7", n_out=4096, p=p)
+    fc7_drop = tf.nn.dropout(fc7, keep_prob, name="fc6_drop")
+
+    fc8 = fc_op(fc7_drop, name="fc8", n_out=1000, p=p)
+
+    softmax = tf.nn.softmax(fc8)
+    predictions = tf.argmax(softmax, 1)
+    return predictions, softmax, fc8, p
+
+
+def inference(images, n_classes):
+    # conv1, shape = [kernel_size, kernel_size, channels, kernel_numbers]
+    with tf.variable_scope("conv1") as scope:
+        weights = tf.get_variable("weights",
+                                  shape=[3, 3, 3, 16],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1, dtype=tf.float32))
+        biases = tf.get_variable("biases",
+                                 shape=[16],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(images, weights, strides=[1, 1, 1, 1], padding="SAME")
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(pre_activation, name="conv1")
+
+    # pool1 && norm1
+    with tf.variable_scope("pooling1_lrn") as scope:
+        pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                               padding="SAME", name="pooling1")
+        norm1 = tf.nn.lrn(pool1, depth_radius=4, bias=1.0, alpha=0.001/9.0,
+                          beta=0.75, name='norm1')
+
+    # conv2
+    with tf.variable_scope("conv2") as scope:
+        weights = tf.get_variable("weights",
+                                  shape=[3, 3, 16, 16],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.1, dtype=tf.float32))
+        biases = tf.get_variable("biases",
+                                 shape=[16],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        conv = tf.nn.conv2d(norm1, weights, strides=[1, 1, 1, 1], padding="SAME")
+        pre_activation = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(pre_activation, name="conv2")
+
+    # pool2 && norm2
+    with tf.variable_scope("pooling2_lrn") as scope:
+        pool2 = tf.nn.max_pool(conv2, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
+                               padding="SAME", name="pooling2")
+        norm2 = tf.nn.lrn(pool2, depth_radius=4, bias=1.0, alpha=0.001/9.0,
+                          beta=0.75, name='norm2')
+
+    # full-connect1
+    with tf.variable_scope("fc1") as scope:
+        reshape = layers.flatten(norm2)
+        dim = reshape.get_shape()[1].value
+        weights = tf.get_variable("weights",
+                                  shape=[dim, 128],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.005, dtype=tf.float32))
+        biases = tf.get_variable("biases",
+                                 shape=[128],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        fc1 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name="fc1")
+
+    # full_connect2
+    with tf.variable_scope("fc2") as scope:
+        weights = tf.get_variable("weights",
+                                  shape=[128, 128],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.005, dtype=tf.float32))
+        biases = tf.get_variable("biases",
+                                 shape=[128],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        fc2 = tf.nn.relu(tf.matmul(fc1, weights) + biases, name="fc2")
+
+    # softmax
+    with tf.variable_scope("softmax_linear") as scope:
+        weights = tf.get_variable("weights",
+                                  shape=[128, n_classes],
+                                  dtype=tf.float32,
+                                  initializer=tf.truncated_normal_initializer(stddev=0.005, dtype=tf.float32))
+        biases = tf.get_variable("biases",
+                                 shape=[n_classes],
+                                 dtype=tf.float32,
+                                 initializer=tf.constant_initializer(0.1))
+        softmax_linear = tf.add(tf.matmul(fc2, weights), biases, name="softmax_linear")
+        # softmax_linear = tf.nn.softmax(softmax_linear)
+
+    return softmax_linear
+
+
+def losses(logits, labels):
+    with tf.variable_scope('loss'):
+        cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits,
+                                                                       labels=labels)
+        loss = tf.reduce_mean(cross_entropy)
+    return loss
+
+
+def evaluation(logits, labels):
+    with tf.variable_scope("accuracy"):
+        correct = tf.nn.in_top_k(logits, labels, 1)
+        correct = tf.cast(correct, tf.float16)
+        accuracy = tf.reduce_mean(correct)
+    return accuracy
